82c440559b308c07f12e29514aa3fd4775458abf
Stephanie Yarboro <stephanieyarboro@gmail.com>
2016-08-03 14:59:29 -0400

    Assignment 20 Menu: completed

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -1,253 +1,4 @@
-# # include AddressBook per usual
-# require_relative "../models/address_book"
-#
-# class MenuController
-#   attr_accessor :address_book
-#
-#   def initialize
-#     @address_book = AddressBook.new
-#   end
-#
-#   def main_menu
-#     # Display the main menu options to command line
-#     puts "Main Menu - #{address_book.entries.count} entries"
-#     puts "1 - View all entries"
-#     puts "2 - Create an entry"
-#     puts "3 - Search for an entry"
-#     puts "4 - Import entries from a CSV"
-#     puts "5 - Exit"
-#     print "Enter your selection: "
-#
-#     # Retrieve user input from command line with GETS
-#     selection = gets.to_i
-#     puts "You picked #{selection}"
-#
-#     # Use case statement operator to determine proper response to user's input
-#     case selection
-#     when 1
-#       system "clear"
-#       view_all_entries
-#       main_menu
-#
-#     when 2
-#       system "clear"
-#       view_entry_number
-#       main_menu
-#
-#     when 3
-#       system "clear"
-#       create_entry
-#       main_menu
-#     when 4
-#       system "clear"
-#       search_entries
-#       main_menu
-#     when 5
-#       system "clear"
-#       read_csv
-#       main_menu
-#     when 6
-#       puts "Good-bye!"
-#       # Terminate them termites (programs) using brexit(0)
-#       exit(0)
-#       # Use an else to catch invalid user input and prompt the user to retry their little hearts out
-#     else
-#       system "clear"
-#       puts "Sorry, that is not a valid input"
-#       main_menu-menu
-#     end
-#   end
-#
-#   # Stub the rest of the methods called in main_menu
-#   # I don't want no stub, stub is a try in my repository
-#   def view_all_entries
-#     # Iterate through all entries in AddressBook using each
-#     @address_book.entries.each do |entry|
-#       system "clear"
-#       puts entry.to_s
-#       # Call entry_submenu to display a submenu for each entry
-#       # submenu, each gets
-#       # subway(menu), eat fresh
-#       entry_submenu(entry)
-#     end
-#
-#     system "clear"
-#     puts "End of entries"
-#   end
-#
-#   def create_entry
-#     # Clear the screen before displaying the create entry prompts
-#     system "clear"
-#     puts "New AddressBloc Entry"
-#     # Use print to prompt the user for each entry's attribute
-#     print "Name: "
-#     name = gets.chomp
-#     print "Phone number: "
-#     phone = gets.chomp
-#     print "Email: "
-#     email = gets.chomp
-#
-#     # Add a new entry to @address_book using add_entry
-#     @address_book.add_entry(name, phone, email)
-#
-#     system "clear"
-#     puts "New entry created"
-#   end
-#
-#   def search_entries
-#   #  print "Search by name: "
-#   #  name = gets.chomp
-#   end
-#
-#     match = @address_book.binary_search.name
-#     system "clear"
-#
-#     if match
-#       puts match.to_s
-#       search_submenu(match)
-#     else
-#       puts "No match found for #{name}"
-#     end
-#   end
-#
-#   def read_csv
-#     print "Enter CSV file to import: "
-#     file_name = gets.chomp
-#
-#     if file_name.empty?
-#       system "clear"
-#       puts "No CSV file read"
-#       main_menu
-#     end
-#
-#     begin
-#       entry_count = @address_book.import_from_csv(file_name)
-#       system "clear"
-#       puts "#{entry_count} new entries added from #{file_name}"
-#     rescue
-#       puts "#{file_name} is not a valid a valid CSV file, please enter the name of a valid CSV file"
-#       read_csv
-#     end
-#   end
-#
-#   def delete_entry(entry)
-#     @address_book.entries.delete(entry)
-#     puts "#{entry.name} has been deleted"
-#   end
-#
-#   def edit_entry(entry)
-#     print "Updated name: "
-#     name = gets.chomp
-#     print "Updated phone number: "
-#     phone_number = gets.chomp
-#     print "Updated email: "
-#     email = gets.chomp
-#
-#     entry.name = name if !name.empty?
-#     entry.phone_number = phone_number if !phone_number.empty?
-#     entry.empty = email if !email.empty?
-#     system "clear"
-#
-#     puts "Updated entry: "
-#     puts entry
-#   end
-#
-#   def entry_submenu(entry)
-#     # Display the submenu optionz
-#     puts "n - next entry"
-#     puts "d - delete entry"
-#     puts "e - edit this entry"
-#     puts "m - return to main menu"
-#
-#     # Chomp chomps (removes) any trailing whitespace from the gets-induced string-a-lings
-#     selection = gets.chomp
-#
-#     case selection
-#       # When user asks to see next entry, our hands are tied
-#       # slash we can do nothing
-#       # henceforth, ze controller returns to view_all_entries
-#     when "n"
-#       # We will handle deleting and editing in another checkpoint... allegedly
-#     when "d"
-#       delete_entry(entry)
-#     when "e"
-#       edit_entry(entry)
-#       entry_submenu(entry)
-#     when "m"
-#       system "clear"
-#       main_menu
-#     else
-#       system "clear"
-#       puts "#{selection} is not a valid input"
-#       entries_submenu(entry)
-#     end
-#   end
-#
-#   def search_submenu(entry)
-#     puts "\nd - delete entry"
-#     puts "e - edit this entry"
-#     puts "m - return to main menu"
-#
-#     selection = gets.chomp
-#
-#     case selection
-#     # when "n"
-#     #   system "clear"
-#     #   add_entry(entry)
-#     #   main_menu
-#     when "d"
-#       system "clear"
-#       delete_entry(entry)
-#       main_menu
-#     when "e"
-#       edit_entry(entry)
-#       system "clear"
-#       main_menu
-#     when "m"
-#       system "clear"
-#       main_menu
-#     else
-#       system "clear"
-#       puts "#{selection} is not a valid input"
-#       puts entry.to_s
-#       search_submenu(entry)
-#     end
-#   end
-# end
-#
-#
-# def entry_submenu(entry)
-#   # Display the submenu options
-#   puts "n - next entry"
-#   puts "d - delete entry"
-#   puts "e - edit this entry"
-#   puts "m - return to main menu"
-#
-#   # Chomp removes any trailing whitespace from the string gets returns
-#     selection = gets.chomp
-#
-#   case selection
-#     # When the user asks to see next entry, we can do nothing, so control us returned to view_all_entries
-#   when "n"
-#     # We will handle deleting and editing in another checkpoint
-#   when "d"
-#   when "e"
-#     # We return the user to the main menu
-#   when "m"
-#     system "clear"
-#     main_menu
-#   else
-#     system "clear"
-#     puts "#{selection} is not a valid input"
-#     entries_submenu(entry)
-#   end
-# end
-# end
-#
-#
-#
-# @@ -0,0 +1,120 @@
-# Include AddressBook using require_relative
+# include AddressBook per usual
 require_relative "../models/address_book"
 
 class MenuController
@@ -264,14 +15,14 @@ class MenuController
     puts "2 - Create an entry"
     puts "3 - Search for an entry"
     puts "4 - Import entries from a CSV"
-    puts "5 - Exit"
+    puts "5 - View Entry Number n"
+    puts "6 - Exit"
     print "Enter your selection: "
 
     # Retrieve user input from the command line with gets
     selection = gets.to_i
-    puts "You picked #{selection}"
+    puts "You selected: #{selection}"
 
-    # Use a case statement operator to determine the proper response to the user's input
     case selection
     when 1
       system "clear"
@@ -290,6 +41,10 @@ class MenuController
       read_csv
       main_menu
     when 5
+      system "clear"
+      view_entry_number_n
+      main_menu
+    when 6
       puts "Good-bye!"
       # Terminate the program using exit(0)
       exit(0)
@@ -297,13 +52,11 @@ class MenuController
     else
       system "clear"
       puts "Sorry, that is not a valid input"
-      main_menu-menu
+      main_menu
     end
   end
 
-  # Stub the rest of the methods called in main_menu
   def view_all_entries
-    # Iterate through all entries in AddressBook using each
     @address_book.entries.each do |entry|
       system "clear"
       puts entry.to_s
@@ -340,6 +93,23 @@ class MenuController
   def read_csv
   end
 
+  def view_entry_number_n
+    #system "clear"
+    puts "What entry do you want to see?"
+    print "Entry number: "
+    selection = gets.chomp.to_i
+
+      if selection < @address_book.entries.count
+        puts @address_book.entries[selection]
+        puts "Please use the return key to revisit main menu."
+        gets.chomp
+        system "clear"
+      else
+        puts "#{selection} is invalid. Please try again."
+        view_entry_number_n
+      end
+  end
+
   def entry_submenu(entry)
     # Display the submenu options
     puts "n - next entry"
@@ -362,8 +132,8 @@ class MenuController
       main_menu
     else
       system "clear"
-      puts "#{selection} is not a valid input"
+      puts "#{selection} is invalid"
       entries_submenu(entry)
     end
   end
-end 
+end
